63e91336bd7af11b0f4599e6bc43ec4376aad199
waleed <waleed.haq@trax.pk>
2023-05-04 15:31:42 +0500

    seap app

---
+++ android/app/src/main/kotlin/com/iconic/socialv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.iconic.saep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/configs.dart
@@ -0,0 +1,46 @@
+/// App Name
+const APP_NAME = "Seap";
+
+/// App Icon src
+const APP_ICON = "assets/app_icon.jpg";
+
+/// Splash screen image src
+const SPLASH_SCREEN_IMAGE = 'assets/images/splash_image.png';
+
+/// OneSignal Notification App Id
+const ONESIGNAL_APP_ID = '08553fea-8c03-4e57-a885-26e94603a522';
+
+/// Todo: Remove Base URL
+/// NOTE: Do not add slash (/) at the end of your domain.
+const DOMAIN_URL = 'https://app.theaestheticone.com';
+const BASE_URL = '$DOMAIN_URL/wp-json/';
+
+/// AppStore Url
+const IOS_APP_LINK = 'https://apps.apple.com/us/app/socialv/id1641646237';
+
+/// Terms and Conditions URL
+const TERMS_AND_CONDITIONS_URL = '$DOMAIN_URL/terms-condition/';
+
+/// Privacy Policy URL
+const PRIVACY_POLICY_URL = '$DOMAIN_URL/privacy-policy-2/';
+
+/// Support URL
+const SUPPORT_URL = 'https://iqonic.desky.support';
+
+/// AdMod Id
+// Android
+const mAdMobAppId = '';
+const mAdMobBannerId = '';
+
+// iOS
+const mAdMobAppIdIOS = '';
+const mAdMobBannerIdIOS = '';
+
+const mTestAdMobBannerId = 'ca-app-pub-3940256099942544/6300978231';
+
+/// Woo Commerce keys
+
+// live
+const CONSUMER_KEY = 'ck_c3ed3e0a4686c125a2647139c3385fd3eee7f5f6';
+const CONSUMER_SECRET = 'cs_6dec957f880dafa76589ef0a488275f73dfba29c';
+
